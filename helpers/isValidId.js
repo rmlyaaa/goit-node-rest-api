4852335c2a21ad9255453b524d923a6d68d59d1b
rmlyaaa <126497199+rmlyaaa@users.noreply.github.com>
2024-02-19 17:58:25 +0100

    isValidId upd

--- helpers/isValidId.js
+++ helpers/isValidId.js
@@ -2,9 +2,9 @@ import { isValidObjectId } from "mongoose";
 import { HttpError } from "./index.js";
 
 const isValidId = (req, res, next) => {
-  const { contactId } = req.params;
-  if (!isValidObjectId(contactId)) {
-    next(HttpError(400, `${contactId} is not valid`));
+  const { id } = req.params;
+  if (!isValidObjectId(id)) {
+    next(HttpError(400, `${id} is not valid`));
   }
   next();
 };
